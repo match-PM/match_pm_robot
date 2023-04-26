28fe8fa31dfc7baa776d962bde9ab63ea13ae679
Florwie <114574121+Florwie@users.noreply.github.com>
2023-04-26 18:08:19 +0200

    structure

--- pm_robot_description/launch/pm_robot_gazebo.launch.py
+++ pm_robot_description/launch/pm_robot_gazebo.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription 
+from launch.actions import IncludeLaunchDescription
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration, TextSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
@@ -11,7 +11,7 @@ from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare 
+from launch_ros.substitutions import FindPackageShare
 import xacro
 
 
@@ -22,30 +22,22 @@ def generate_launch_description():
     file_subpath = 'urdf/pm_robot_main.xacro'
 
     # Use xacro to process the file
-    pm_main_xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+    pm_main_xacro_file = os.path.join(
+        get_package_share_directory(pkg_name), file_subpath)
 
-    pm_robot_configuration = {  'with_Tool_MPG_10'                  :'true',
-                                'with_Tool_MPG_10_Jaw_3mm_Lens'     :'true',
-                                'with_Gonio_Right'                  :'true',
-                                'with_Gonio_Left'                   :'true',
-                           }
+    pm_robot_configuration = {'with_Tool_MPG_10': 'true',
+                              'with_Tool_MPG_10_Jaw_3mm_Lens': 'true',
+                              'with_Gonio_Right': 'true',
+                              'with_Gonio_Left': 'true',
+                              }
 
-    robot_description_raw = xacro.process_file(pm_main_xacro_file, 
+    robot_description_raw = xacro.process_file(pm_main_xacro_file,
                                                mappings={
-                                                'with_Tool_MPG_10': str(pm_robot_configuration['with_Tool_MPG_10']),
-                                                'with_Gonio_Left': str(pm_robot_configuration['with_Gonio_Left']), 
-                                                'with_Gonio_Right': str(pm_robot_configuration['with_Gonio_Right']),   
-                                                'with_Tool_MPG_10_Jaw_3mm_Lens': str(pm_robot_configuration['with_Tool_MPG_10_Jaw_3mm_Lens']),           
-                                                }).toxml()
-    
-
-    robot_controllers = PathJoinSubstitution(
-        [
-            FindPackageShare("pm_robot_description"),
-            "config",
-            "control.yaml",
-        ]
-    )
+                                                   'with_Tool_MPG_10': str(pm_robot_configuration['with_Tool_MPG_10']),
+                                                   'with_Gonio_Left': str(pm_robot_configuration['with_Gonio_Left']),
+                                                   'with_Gonio_Right': str(pm_robot_configuration['with_Gonio_Right']),
+                                                   'with_Tool_MPG_10_Jaw_3mm_Lens': str(pm_robot_configuration['with_Tool_MPG_10_Jaw_3mm_Lens']),
+                                               }).toxml()
 
     # Configure the node
     robot_state_publisher_node = Node(
@@ -53,58 +45,36 @@ def generate_launch_description():
         executable='robot_state_publisher',
         output='screen',
         parameters=[{'robot_description': robot_description_raw,
-        'use_sim_time': True}] # add other parameters here if required
+                     'use_sim_time': True}]  # add other parameters here if required
     )
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-        )
+    )
 
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                    arguments=['-topic', 'robot_description',
-                                '-entity', 'pm_robot'],
-                    output='screen')
-    
-    spawn_entity_Gripper = Node(package='gazebo_ros', executable='spawn_entity.py',
-                    namespace="pm_robot_gripper",         
-                    arguments=['-topic', 'robot_description',
-                                '-entity', 'pm_robot_gripper'],
-                    output='screen')
-
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=['robot_description', robot_controllers],
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        },
-    )
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'pm_robot'],
+                        output='screen')
 
     robot_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        arguments= ["joint_trajectory_controller"],
+        arguments=["joint_trajectory_controller"],
     )
 
     robot_controller_forward_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        arguments= ["forward_position_controller"],
+        arguments=["forward_position_controller"],
     )
 
     joint_broad_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        #namespace='pm_robot',
-        arguments= ["joint_state_broadcaster"],
-    )
-
-    joint_broad_spawner2 = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments= ["joint_state_broadcaster2"],
+        # namespace='pm_robot',
+        arguments=["joint_state_broadcaster"],
     )
 
     # Delay start of robot_controller after `joint_state_broadcaster`
@@ -123,28 +93,13 @@ def generate_launch_description():
         )
     )
 
-   # Delay start of robot_controller after `joint_state_broadcaster`
-    delay_spawn_entity_1 = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=spawn_entity,
-            on_exit=[robot_state_publisher_node],
-        )
-    )
-
     # Run the node
     return LaunchDescription([
         gazebo,
-        #control_node,
         robot_state_publisher_node,
         joint_broad_spawner,
         spawn_entity,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
         delay_robot_controller_spawner_after_controller,
-        
-        #robot_state_publisher_node_Gripper,
-        #spawn_entity_Gripper,
-        #joint_broad_spawner2,
 
     ])
-
-
