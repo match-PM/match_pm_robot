696cf2c34e1f2d510f0a63ffd53d9f1b14f005f9
tereiATmatch <terei@match.uni-hannover.de>
2023-05-04 11:05:53 +0200

    Moveit + Gazebo working

--- pm_robot_description/launch/pm_robot_gazebo.launch.py
+++ pm_robot_description/launch/pm_robot_gazebo.launch.py
@@ -10,7 +10,7 @@ from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_move_group_launch
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
-
+from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 import xacro
@@ -26,16 +26,19 @@ def generate_launch_description():
     pm_main_xacro_file = os.path.join(
         get_package_share_directory(pkg_name), file_subpath)
 
-    pm_robot_configuration = {'with_Tool_MPG_10':               'false',
-                              'with_Tool_MPG_10_Jaw_3mm_Lens':  'false',
-                              'with_Gonio_Right':               'true',
-                              'with_Gonio_Left':                'true',
-                              'with_Tool_SPT_Holder':           'false',
-                              'with_SPT_R_A1000_I500':          'false',
+    pm_robot_configuration = {
+                                'launch_mode':                    'sim_HW',              #real_HW sim_HW fake_HW real_sim_HW
+                                'with_Tool_MPG_10':               'false',
+                                'with_Tool_MPG_10_Jaw_3mm_Lens':  'false',
+                                'with_Gonio_Right':               'true',
+                                'with_Gonio_Left':                'true',
+                                'with_Tool_SPT_Holder':           'true',
+                                'with_SPT_R_A1000_I500':          'false',
                               }
 
     robot_description_raw = xacro.process_file(pm_main_xacro_file,
                                                mappings={
+                                                   'launch_mode': str(pm_robot_configuration['launch_mode']),
                                                    'with_Tool_MPG_10': str(pm_robot_configuration['with_Tool_MPG_10']),
                                                    'with_Gonio_Left': str(pm_robot_configuration['with_Gonio_Left']),
                                                    'with_Gonio_Right': str(pm_robot_configuration['with_Gonio_Right']),
@@ -48,19 +51,53 @@ def generate_launch_description():
     moveit_config = (
         MoveItConfigsBuilder("pm_robot", package_name="pm_robot_moveit_config")
         #.robot_description(robot_description_raw) not working
-        .robot_description(file_path="config/pm_robot_link.urdf.xacro")
+        #.robot_description(file_path="config/pm_robot_link.urdf.xacro")
         #.robot_description(file_path="config/pm_robot.urdf.xacro")
+        .robot_description(file_path=pm_main_xacro_file,
+                                               mappings={
+                                                   'launch_mode': str(pm_robot_configuration['launch_mode']),
+                                                   'with_Tool_MPG_10': str(pm_robot_configuration['with_Tool_MPG_10']),
+                                                   'with_Gonio_Left': str(pm_robot_configuration['with_Gonio_Left']),
+                                                   'with_Gonio_Right': str(pm_robot_configuration['with_Gonio_Right']),
+                                                   'with_Tool_MPG_10_Jaw_3mm_Lens': str(pm_robot_configuration['with_Tool_MPG_10_Jaw_3mm_Lens']),
+                                                   'with_Tool_SPT_Holder': str(pm_robot_configuration['with_Tool_SPT_Holder']),
+                                                   'with_SPT_R_A1000_I500': str(pm_robot_configuration['with_SPT_R_A1000_I500']),
+                                               })
         .robot_description_semantic(file_path="config/pm_robot.srdf")
         .trajectory_execution(file_path="config/moveit_controllers.yaml")
         .to_moveit_configs()
     )
+
+    move_group_configuration = {
+        "publish_robot_description_semantic": True,
+        #"allow_trajectory_execution": LaunchConfiguration("allow_trajectory_execution"),
+        # Note: Wrapping the following values is necessary so that the parameter value can be the empty string
+        #"capabilities": ParameterValue(
+        #    LaunchConfiguration("capabilities"), value_type=str
+        #),
+        #"disable_capabilities": ParameterValue(
+        #    LaunchConfiguration("disable_capabilities"), value_type=str
+        #),
+        # Publish the planning scene of the physical robot so that rviz plugin can know actual robot
+        #"publish_planning_scene": should_publish,
+        #"publish_geometry_updates": should_publish,
+        #"publish_state_updates": should_publish,
+        #"publish_transforms_updates": should_publish,
+        #"monitor_dynamics": False,
+        "use_sim_time": True,
+    }    
+
+    move_group_params = [
+        moveit_config.to_dict(),
+        move_group_configuration,
+    ]
 
     # Start the actual move_group node/action server
     run_move_group_node = Node(
         package="moveit_ros_move_group",
         executable="move_group",
         output="screen",
-        parameters=[moveit_config.to_dict()],
+        parameters=move_group_params,
     )
 
     # RViz
@@ -96,6 +133,14 @@ def generate_launch_description():
         #parameters=[moveit_config.robot_description],
     )
 
+    robot_state_publisher_node_mov = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[moveit_config.robot_description],
+    )
+
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
@@ -114,7 +159,8 @@ def generate_launch_description():
         ]
     )
 
-    ros2_controllers_path = os.path.join(
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path_mov = os.path.join(
         get_package_share_directory("pm_robot_moveit_config"),
         "config",
         "ros2_controllers.yaml",
@@ -123,10 +169,21 @@ def generate_launch_description():
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description_raw, robot_controllers_path],
+        parameters=[moveit_config.robot_description, robot_controllers_path],
         output="both",
     )
 
+    joint_broad_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        # namespace='pm_robot',
+        #arguments=["joint_state_broadcaster"], previous
+        arguments=[
+            "pm_robot_joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
 
     robot_controller_spawner = Node(
         package='controller_manager',
@@ -150,17 +207,6 @@ def generate_launch_description():
         ],
     )
 
-    joint_broad_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        # namespace='pm_robot',
-        #arguments=["joint_state_broadcaster"], previous
-        arguments=[
-            "pm_robot_joint_state_broadcaster",
-            "--controller-manager",
-            "/controller_manager",
-        ],
-    )
 
     # Delay start of robot_controller after `joint_state_broadcaster`
     delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
@@ -178,17 +224,19 @@ def generate_launch_description():
         )
     )
 
-    # Run the node
-    return LaunchDescription([
-        
-        rviz_node,
-        gazebo,
-        robot_state_publisher_node,
-        run_move_group_node,
-        control_node,
-        joint_broad_spawner,
-        spawn_entity,
-        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
-        delay_robot_controller_spawner_after_controller,
+    # Define Launch Description
 
-    ])
+    ld = LaunchDescription()
+    ld.add_action(rviz_node)
+    if (str(pm_robot_configuration['launch_mode']) == 'sim_HW'):
+        ld.add_action(gazebo)
+    ld.add_action(robot_state_publisher_node_mov)
+    ld.add_action(run_move_group_node)
+    ld.add_action(control_node)
+    ld.add_action(joint_broad_spawner)
+    if (str(pm_robot_configuration['launch_mode']) == 'sim_HW'):
+        ld.add_action(spawn_entity)
+    ld.add_action(delay_robot_controller_spawner_after_joint_state_broadcaster_spawner)
+    ld.add_action(delay_robot_controller_spawner_after_controller)
+
+    return ld
