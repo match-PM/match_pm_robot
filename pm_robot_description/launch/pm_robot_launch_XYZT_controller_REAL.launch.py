1270cb6f86a1d184d27d7ddb815f9c78da35a13f
Florwie <114574121+Florwie@users.noreply.github.com>
2023-06-12 17:13:31 +0200

    bringup fixes

--- pm_robot_description/launch/pm_robot_launch_XYZT_controller_REAL.launch.py
+++ pm_robot_description/launch/pm_robot_launch_XYZT_controller_REAL.launch.py
@@ -9,7 +9,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     # XYZ Axis Joint State Broadcaster
-    Spawn_pm_robot_JSB = Node(
+    Spawn_pm_robot_broadcaster = Node(
         package='controller_manager',
         executable='spawner',
         # namespace='pm_robot',
@@ -21,58 +21,58 @@ def generate_launch_description():
     )
 
     # XYZ Axis Joint Trajectory Controller
-    Spawn_pm_robot_xyz_axis_FPC = Node(
+    Spawn_pm_robot_xyz_axis_controller = Node(
         package='controller_manager',
         executable='spawner',
         arguments=[
-            "pm_robot_xyz_axis_FPC",
+            "pm_robot_xyz_axis_controller",
             "--controller-manager",
             "/controller_manager",
         ],
     )
 
     # T Axis Joint Trajectory Controller
-    Spawn_pm_robot_t_axis_FPC = Node(
+    Spawn_pm_robot_t_axis_controller = Node(
         package='controller_manager',
         executable='spawner',
         arguments=[
-            "pm_robot_xyz_axis_FPC",
+            "pm_robot_t_axis_controller",
             "--controller-manager",
             "/controller_manager",
         ],
     )
 
-    Spawn_pm_robot_io_axis_FPC = Node(
+    Spawn_pm_robot_io_axis_controller = Node(
         package='controller_manager',
         executable='spawner',
         arguments=[
-            "pm_robot_IO_Axis_FPC",
+            "pm_robot_IO_Axis_controller",
             "--controller-manager",
             "/controller_manager",
         ],
     )
 
     # Delay start of robot_controller after `joint_state_broadcaster`
-    delay_Spawn_pm_robot_xyz_FPC = RegisterEventHandler(
+    delay_Spawn_pm_robot_xyz_JTC = RegisterEventHandler(
         event_handler=OnProcessExit(
-            target_action=Spawn_pm_robot_JSB,
-            on_exit=[Spawn_pm_robot_xyz_axis_FPC],
+            target_action=Spawn_pm_robot_broadcaster,
+            on_exit=[Spawn_pm_robot_xyz_axis_controller],
         )
     )
 
     # Delay start of robot_controller after `joint_state_broadcaster`
-    delay_Spawn_pm_robot_t_axis_FPC = RegisterEventHandler(
+    delay_Spawn_pm_robot_t_axis_JTC = RegisterEventHandler(
         event_handler=OnProcessExit(
-            target_action=Spawn_pm_robot_JSB,
-            on_exit=[Spawn_pm_robot_t_axis_FPC],
+            target_action=Spawn_pm_robot_broadcaster,
+            on_exit=[Spawn_pm_robot_t_axis_controller],
         )
     )
 
     # Delay start of robot_controller after `joint_state_broadcaster`
     delay_robot_controller_spawner_after_controller = RegisterEventHandler(
         event_handler=OnProcessExit(
-            target_action=Spawn_pm_robot_JSB,
-            on_exit=[Spawn_pm_robot_io_axis_FPC],
+            target_action=Spawn_pm_robot_broadcaster,
+            on_exit=[Spawn_pm_robot_io_axis_controller],
         )
     )
 
@@ -80,9 +80,9 @@ def generate_launch_description():
 
     ld = LaunchDescription()
 
-    ld.add_action(Spawn_pm_robot_JSB)    
-    ld.add_action(delay_Spawn_pm_robot_xyz_FPC)
-    # ld.add_action(delay_Spawn_pm_robot_t_axis_FPC)
-    # ld.add_action(delay_robot_controller_spawner_after_controller)
+    ld.add_action(Spawn_pm_robot_broadcaster)    
+    ld.add_action(delay_Spawn_pm_robot_xyz_JTC)
+    ld.add_action(delay_Spawn_pm_robot_t_axis_JTC)
+    #ld.add_action(delay_robot_controller_spawner_after_controller)
 
     return ld
--- pm_robot_description/launch/pm_robot_launch_gonio_left_controller.launch.py
+++ pm_robot_description/launch/pm_robot_launch_gonio_left_controller.launch.py
@@ -20,21 +20,21 @@ def generate_launch_description():
         ],
     )
     # XYZ Axis Joint Trajectory Controller
-    Spawn_pm_robot_gonio_left_JTC = Node(
+    Spawn_pm_robot_gonio_left_controller = Node(
         package='controller_manager',
         executable='spawner',
         arguments=[
-            "pm_robot_gonio_left_JTC",
+            "pm_robot_gonio_left_controller",
             "--controller-manager",
             "/controller_manager",
         ],
     )
 
     # Delay start of robot_controller after `joint_state_broadcaster`
-    delay_Spawn_pm_robot_gonio_left_JTC = RegisterEventHandler(
+    delay_Spawn_pm_robot_gonio_left_controller = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=Spawn_pm_robot_gonio_left_JSB,
-            on_exit=[Spawn_pm_robot_gonio_left_JTC],
+            on_exit=[Spawn_pm_robot_gonio_left_controller],
         )
     )
 
@@ -42,5 +42,5 @@ def generate_launch_description():
 
     ld = LaunchDescription()
     ld.add_action(Spawn_pm_robot_gonio_left_JSB)
-    ld.add_action(delay_Spawn_pm_robot_gonio_left_JTC)
+    ld.add_action(delay_Spawn_pm_robot_gonio_left_controller)
     return ld
