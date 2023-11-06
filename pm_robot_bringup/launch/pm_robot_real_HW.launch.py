a77b564bae885436043e06254f469445ca1e8d98
Florwie <114574121+Florwie@users.noreply.github.com>
2023-11-06 18:10:41 +0100

    urdf changes hw_interface fix

--- pm_robot_bringup/launch/pm_robot_real_HW.launch.py
+++ pm_robot_bringup/launch/pm_robot_real_HW.launch.py
@@ -2,7 +2,13 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration, TextSubstitution
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    PathJoinSubstitution,
+    LaunchConfiguration,
+    TextSubstitution,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_move_group_launch
@@ -17,39 +23,45 @@ import xacro
 import yaml
 from yaml.loader import SafeLoader
 import sys
-from launch.substitutions import Command 
+from launch.substitutions import Command
+
 
 def generate_launch_description():
+    bringup_config_path = os.path.join(
+        get_package_share_directory("pm_robot_bringup"),
+        "config/pm_robot_bringup_config.yaml",
+    )
 
-    bringup_config_path = os.path.join(get_package_share_directory('pm_robot_bringup'), 'config/pm_robot_bringup_config.yaml')
-    
     f = open(bringup_config_path)
-    bringup_config = yaml.load(f,Loader=SafeLoader)
+    bringup_config = yaml.load(f, Loader=SafeLoader)
     f.close()
 
     # Specify the name of the package and path to xacro file within the package
-    pkg_name = 'pm_robot_description'
-    file_subpath = 'urdf/pm_robot_main.xacro'
+    pkg_name = "pm_robot_description"
+    file_subpath = "urdf/pm_robot_main.xacro"
 
     # Use xacro to process the file
-    pm_main_xacro_file = os.path.join(get_package_share_directory(pkg_name), file_subpath)
+    pm_main_xacro_file = os.path.join(
+        get_package_share_directory(pkg_name), file_subpath
+    )
 
     launch_moveit = True
 
-    
     sim_time = False
 
-    mappings={
-        'launch_mode': 'sim_HW',
-        #'with_Tool_MPG_10': str(bringup_config['pm_robot_tools']['MPG_10']['with_Tool_MPG_10']),
-        'with_Gonio_Left': str(bringup_config['pm_robot_gonio_left']['with_Gonio_Left']),
-        'with_Gonio_Right': str(bringup_config['pm_robot_gonio_right']['with_Gonio_Right']),
-        #'with_Tool_MPG_10_Jaw_3mm_Lens': str(bringup_config['pm_robot_tools']['MPG_10']['Config']['with_Tool_MPG_10_Jaw_3mm_Lens']),
-        #'with_Tool_SPT_Holder': str(bringup_config['pm_robot_tools']['SPT_Tool_Holder']['with_Tool_SPT_Holder']),
-        #'with_SPT_R_A1000_I500': str(bringup_config['pm_robot_tools']['SPT_Tool_Holder']['Config']['with_SPT_R_A1000_I500']),
+    mappings = {
+        "launch_mode": "real_HW",
+        "with_Gonio_Left": str(
+            bringup_config["pm_robot_gonio_left"]["with_Gonio_Left"]
+        ),
+        "with_Gonio_Right": str(
+            bringup_config["pm_robot_gonio_right"]["with_Gonio_Right"]
+        ),
     }
 
-    robot_description_raw = xacro.process_file(pm_main_xacro_file, mappings=mappings).toxml()
+    robot_description_raw = xacro.process_file(
+        pm_main_xacro_file, mappings=mappings
+    ).toxml()
 
     controler_param = PathJoinSubstitution(
         [
@@ -59,45 +71,50 @@ def generate_launch_description():
         ]
     )
     forward_command_action_server = Node(
-            package="pm_robot_control_test",
-            executable="forward_command_action_server",
-            #name="pm_robot_IO_Axis_controller",
-            parameters=[{'controller_param': controler_param}, 
-                        {'robot_description': robot_description_raw}],
-            output={
-                "stdout": "screen",
-                "stderr": "screen",
-            },
+        package="pm_robot_control_test",
+        executable="forward_command_action_server",
+        # name="pm_robot_IO_Axis_controller",
+        parameters=[
+            {"controller_param": controler_param},
+            {"robot_description": robot_description_raw},
+        ],
+        output={
+            "stdout": "screen",
+            "stderr": "screen",
+        },
     )
 
     moveit_config = (
         MoveItConfigsBuilder("pm_robot", package_name="pm_robot_moveit_config")
-        .robot_description(file_path=pm_main_xacro_file,mappings=mappings)
+        .robot_description(file_path=pm_main_xacro_file, mappings=mappings)
         .robot_description_semantic(file_path="config/pm_robot.srdf")
         .trajectory_execution(file_path="config/moveit_controllers.yaml")
         .robot_description_kinematics(file_path="config/kinematics.yaml")
-        .planning_pipelines(pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"])
+        .planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
+        )
         .to_moveit_configs()
     )
 
     move_group_configuration = {
         "publish_robot_description_semantic": True,
-        #"allow_trajectory_execution": LaunchConfiguration("allow_trajectory_execution"),
+        "trajectory_execution.allowed_execution_duration_scaling": 10.0,
+        # "allow_trajectory_execution": LaunchConfiguration("allow_trajectory_execution"),
         # Note: Wrapping the following values is necessary so that the parameter value can be the empty string
-        #"capabilities": ParameterValue(
+        # "capabilities": ParameterValue(
         #    LaunchConfiguration("capabilities"), value_type=str
-        #),
-        #"disable_capabilities": ParameterValue(
+        # ),
+        # "disable_capabilities": ParameterValue(
         #    LaunchConfiguration("disable_capabilities"), value_type=str
-        #),
+        # ),
         # Publish the planning scene of the physical robot so that rviz plugin can know actual robot
         "publish_planning_scene": "should_publish",
-        #"publish_geometry_updates": should_publish,
-        #"publish_state_updates": "should_publish",
-        #"publish_transforms_updates": should_publish,
-        #"monitor_dynamics": False,
+        # "publish_geometry_updates": should_publish,
+        # "publish_state_updates": "should_publish",
+        # "publish_transforms_updates": should_publish,
+        # "monitor_dynamics": False,
         "use_sim_time": sim_time,
-    }    
+    }
 
     move_group_params = [
         moveit_config.to_dict(),
@@ -136,13 +153,14 @@ def generate_launch_description():
 
     # Configure the node
     robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
         name="robot_state_publisher",
-        output='both',
-        parameters=[{'robot_description': robot_description_raw,
-                     'use_sim_time': sim_time}]  # add other parameters here if required
-        #parameters=[moveit_config.robot_description],
+        output="both",
+        parameters=[
+            {"robot_description": robot_description_raw, "use_sim_time": sim_time}
+        ]  # add other parameters here if required
+        # parameters=[moveit_config.robot_description],
     )
 
     robot_state_publisher_node_mov = Node(
@@ -177,8 +195,10 @@ def generate_launch_description():
         ]
     )
 
-    robot_description_command = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-    
+    robot_description_command = Command(
+        ["ros2 param get --hide-type /robot_state_publisher robot_description"]
+    )
+
     # control_manager = Node(
     #     package="controller_manager",
     #     executable="ros2_control_node",
@@ -189,26 +209,39 @@ def generate_launch_description():
     control_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description':robot_description_command},
-                    robot_controllers_path,robot_gonio_left_controllers_path,robot_gonio_right_controllers_path],
+        parameters=[
+            {"robot_description": robot_description_command},
+            robot_controllers_path,
+            robot_gonio_left_controllers_path,
+            robot_gonio_right_controllers_path,
+        ],
         output="both",
+        # arguments=[                   # Set log level to debug
+        #     "--ros-args",
+        #     "--log-level",
+        #     "debug",
+        # ],
     )
 
     delayed_controller_manager = TimerAction(period=3.0, actions=[control_manager])
 
     launch_XYZT_controllers = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare(pkg_name),
-                'launch',
-                'pm_robot_launch_XYZT_controller_REAL.launch.py'
-                ])
-        ])#,
-            #launch_arguments={                 # das muesste so funktionieren, noch nicht getestet; Ros2 control braucht aber use_sim_time nicht, da in yaml gegeben
-            #    'use_sim_time': sim_time,
-            #}.items()
-        )
-    
+        PythonLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare(pkg_name),
+                        "launch",
+                        "pm_robot_launch_XYZT_controller_REAL.launch.py",
+                    ]
+                )
+            ]
+        )  # ,
+        # launch_arguments={                 # das muesste so funktionieren, noch nicht getestet; Ros2 control braucht aber use_sim_time nicht, da in yaml gegeben
+        #    'use_sim_time': sim_time,
+        # }.items()
+    )
+
     pm_lights_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
@@ -238,54 +271,69 @@ def generate_launch_description():
             "/controller_manager",
         ],
     )
-    
-    launch_gonio_left_controller = IncludeLaunchDescription(PythonLaunchDescriptionSource([
-        PathJoinSubstitution([
-            FindPackageShare(pkg_name),
-            'launch',
-            'pm_robot_launch_gonio_left_controller.launch.py'
-            ])
-        ])
+
+    launch_gonio_left_controller = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare(pkg_name),
+                        "launch",
+                        "pm_robot_launch_gonio_left_controller.launch.py",
+                    ]
+                )
+            ]
         )
-    
-    launch_gonio_right_controller = IncludeLaunchDescription(PythonLaunchDescriptionSource([
-        PathJoinSubstitution([
-            FindPackageShare(pkg_name),
-            'launch',
-            'pm_robot_launch_gonio_right_controller.launch.py'
-            ])
-        ])
+    )
+
+    launch_gonio_right_controller = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare(pkg_name),
+                        "launch",
+                        "pm_robot_launch_gonio_right_controller.launch.py",
+                    ]
+                )
+            ]
         )
-    
-    launch_gonio_parallel_gripper_controller = IncludeLaunchDescription(PythonLaunchDescriptionSource([
-        PathJoinSubstitution([
-            FindPackageShare(pkg_name),
-            'launch',
-            'pm_robot_launch_gripper_controller.launch.py'
-            ])
-        ])
-        )    
-    
+    )
+
+    launch_gonio_parallel_gripper_controller = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare(pkg_name),
+                        "launch",
+                        "pm_robot_launch_gripper_controller.launch.py",
+                    ]
+                )
+            ]
+        )
+    )
+
     # Define Launch Description
     ld = LaunchDescription()
 
     ld.add_action(robot_state_publisher_node)
-    #ld.add_action(control_manager)
+    # ld.add_action(control_manager)
     ld.add_action(delayed_controller_manager)
     if launch_moveit:
         ld.add_action(rviz_node)
         ld.add_action(run_move_group_node)
     ld.add_action(launch_XYZT_controllers)
 
-    if (mappings['with_Gonio_Left'] == 'True'):
+    if mappings["with_Gonio_Left"] == "True":
         ld.add_action(launch_gonio_left_controller)
-    if (mappings['with_Gonio_Right'] == 'True'):
+    if mappings["with_Gonio_Right"] == "True":
         ld.add_action(launch_gonio_right_controller)
 
     ld.add_action(pm_lights_controller_spawner)
     ld.add_action(pm_pneumatic_controller_spawner)
     ld.add_action(pm_nozzle_controller_spawner)
-   
+
     # if (str(mappings['with_Tool_MPG_10']) == 'true'):
     #     ld.add_action(launch_gonio_parallel_gripper_controller)
     return ld
